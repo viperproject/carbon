e8c551040c6337bb486b20f99f2c380aa668c3b2
Stefan Heule <stefanheule@gmail.com>
2013-02-21 18:53:57 +0100

    Initial project configuration.

---
+++ project/build.scala
@@ -0,0 +1,29 @@
+import sbt._
+import Keys._
+
+object CarbonBuild extends Build {
+  lazy val baseSettings = (
+       Defaults.defaultSettings
+    ++ Seq(
+          organization := "semper",
+          version := "1.0-SNAPSHOT",
+          scalaVersion := "2.10.0",
+          scalacOptions in Compile ++= Seq("-deprecation", "-unchecked", "-feature"))
+  )
+
+  lazy val carbon = {
+    Project(
+      id = "carbon",
+      base = file("."),
+      settings = (
+           baseSettings
+        ++ Seq(
+              name := "Carbon",
+              testOptions in Test += Tests.Argument(TestFrameworks.ScalaTest, "-oD"),
+              traceLevel := 10,
+              maxErrors := 6,
+              classDirectory in Test <<= classDirectory in Compile,
+              libraryDependencies ++= Seq()))
+    ).dependsOn(RootProject(new java.io.File("../sil")))
+  }
+}
