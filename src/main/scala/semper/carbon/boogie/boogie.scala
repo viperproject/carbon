e23e8bcb374d482fe86a848dbf0115e8e69b0b04
Stefan Heule <stefanheule@gmail.com>
2013-02-23 18:10:05 +0100

    More Boogie AST nodes and more type-safety for some existing nodes.

--- src/main/scala/semper/carbon/boogie/boogie.scala
+++ src/main/scala/semper/carbon/boogie/boogie.scala
@@ -15,8 +15,9 @@ sealed trait Node {
 }
 
 /** A local variable declaration.  Note that this is not a statement, as local variables do not have to be declared. */
-case class LocalVarDecl(name: String, typ: Type, where: Option[Exp] = None) extends Node
+case class LocalVarDecl(name: String, typ: Type, where: Option[MaybeBool] = None) extends Node
 
+case class Lbl(name: String) extends Node
 
 // --- Types
 
@@ -32,8 +33,8 @@ case class NamedType(name: String, typArgs: Seq[TypeVar]) extends Type
 // --- Expressions
 
 sealed trait Exp extends Node with PrettyExpression {
-  def ===(other: MaybeInt) = BinExp(this, EqCmp, other)
-  def !==(other: MaybeInt) = BinExp(this, NeCmp, other)
+  def ===(other: Exp) = BinExp(this, EqCmp, other)
+  def !==(other: Exp) = BinExp(this, NeCmp, other)
 }
 
 /** A common trait for expressions that one can assign a new value to. */
@@ -42,32 +43,38 @@ sealed trait Lhs extends Exp {
 }
 
 /** A trait for expressions that might be of type int. */
-sealed trait MaybeInt extends Exp {
-  def +(other: MaybeInt) = BinExp(this, Add, other)
-  def -(other: MaybeInt) = BinExp(this, Sub, other)
-  def *(other: MaybeInt) = BinExp(this, Mul, other)
-  def /(other: MaybeInt) = BinExp(this, Div, other)
-  def %(other: MaybeInt) = BinExp(this, Mod, other)
-  def <(other: MaybeInt) = BinExp(this, LtCmp, other)
-  def >(other: MaybeInt) = BinExp(this, GtCmp, other)
-  def <=(other: MaybeInt) = BinExp(this, LeCmp, other)
-  def >=(other: MaybeInt) = BinExp(this, GeCmp, other)
+sealed trait MaybeNum extends Exp {
+  def +(other: MaybeNum) = BinExp(this, Add, other)
+  def -(other: MaybeNum) = BinExp(this, Sub, other)
+  def *(other: MaybeNum) = BinExp(this, Mul, other)
+  def /(other: MaybeNum) = BinExp(this, Div, other)
+  def %(other: MaybeNum) = BinExp(this, Mod, other)
+  def <(other: MaybeNum) = BinExp(this, LtCmp, other)
+  def >(other: MaybeNum) = BinExp(this, GtCmp, other)
+  def <=(other: MaybeNum) = BinExp(this, LeCmp, other)
+  def >=(other: MaybeNum) = BinExp(this, GeCmp, other)
+  def neg = UnExp(Neg, this)
 }
 
 /** A trait for expressions that might be of type bool. */
 sealed trait MaybeBool extends Exp {
-  def &&(other: MaybeInt) = BinExp(this, And, other)
-  def ||(other: MaybeInt) = BinExp(this, Or, other)
-  def ==>(other: MaybeInt) = BinExp(this, Implies, other)
-  def <==>(other: MaybeInt) = BinExp(this, Equiv, other)
+  def &&(other: MaybeBool) = BinExp(this, And, other)
+  def ||(other: MaybeBool) = BinExp(this, Or, other)
+  def ==>(other: MaybeBool) = BinExp(this, Implies, other)
+  def <==>(other: MaybeBool) = BinExp(this, Equiv, other)
   def forall(vars: Seq[LocalVarDecl], triggers: Seq[Trigger]) =
     Forall(vars, triggers, this)
   def exists(vars: Seq[LocalVarDecl]) =
     Exists(vars, this)
+  def not = UnExp(Not, this)
+  def thn(thn: Exp) = PartialCondExp(this, thn)
+}
+case class PartialCondExp(cond: MaybeBool, thn: Exp) {
+  def els(els: Exp) = CondExp(cond, thn, els)
 }
 
-case class IntLit(i: BigInt) extends Exp with MaybeInt
-case class RealLit(d: Double) extends Exp
+case class IntLit(i: BigInt) extends MaybeNum
+case class RealLit(d: Double) extends MaybeNum
 case class TrueLit() extends BoolLit(true)
 case class FalseLit() extends BoolLit(false)
 sealed abstract class BoolLit(val b: Boolean) extends Exp with MaybeBool
@@ -75,10 +82,16 @@ object BoolLit {
   def unapply(b: BoolLit) = Some(b.b)
 }
 
-case class BinExp(left: Exp, binop: BinOp, right: Exp) extends Exp with PrettyBinaryExpression with MaybeInt with MaybeBool {
+case class BinExp(left: Exp, binop: BinOp, right: Exp) extends Exp with PrettyBinaryExpression with MaybeNum with MaybeBool {
   lazy val op = binop.name
   lazy val priority = binop.priority
   lazy val fixity = binop.fixity
+}
+
+case class UnExp(unop: UnOp, exp: Exp) extends Exp with PrettyUnaryExpression with MaybeNum with MaybeBool {
+  lazy val op = unop.name
+  lazy val priority = unop.priority
+  lazy val fixity = unop.fixity
 }
 
 sealed abstract class BinOp(val name: String, val priority: Int, val fixity: Fixity)
@@ -99,7 +112,8 @@ case object EqCmp extends RelOp("==")
 case object NeCmp extends RelOp("≠" or "!=")
 
 case object Or extends BinOp("∨" or "||", 3, Infix(LeftAssoc))
-case object And extends BinOp("∧" or "&&", 3, Infix(LeftAssoc)) // Note: Boogie uses the same priority for 'and' and 'or'.
+case object And extends BinOp("∧" or "&&", 3, Infix(LeftAssoc))
+// Note: Boogie uses the same priority for 'and' and 'or'.
 case object Implies extends BinOp("⇒" or "==>", 4, Infix(RightAssoc))
 case object Equiv extends BinOp("⇔" or "<==>", 5, Infix(NonAssoc))
 
@@ -111,28 +125,42 @@ sealed trait QuantifiedExp extends Exp with MaybeBool {
   def vars: Seq[LocalVarDecl]
   def exp: Exp
 }
-case class Forall(vars: Seq[LocalVarDecl], triggers: Seq[Trigger], exp: Exp) extends QuantifiedExp
-case class Exists(vars: Seq[LocalVarDecl], exp: Exp) extends QuantifiedExp
+case class Forall(vars: Seq[LocalVarDecl], triggers: Seq[Trigger], exp: MaybeBool) extends QuantifiedExp
+case class Exists(vars: Seq[LocalVarDecl], exp: MaybeBool) extends QuantifiedExp
 case class Trigger(exps: Seq[Exp]) extends Node
 
-case class LocalVar(name: String, typ: Type) extends Exp with Lhs with MaybeInt with MaybeBool
-case class FuncApp(name: String, args: Seq[Exp]) extends Exp with MaybeInt with MaybeBool
-case class MapSelect(map: Exp, idxs: Seq[Exp]) extends Exp with Lhs with MaybeInt with MaybeBool
+case class CondExp(cond: MaybeBool, thn: Exp, els: Exp) extends MaybeBool
+
+case class LocalVar(name: String, typ: Type) extends Lhs with MaybeNum with MaybeBool
+case class FuncApp(name: String, args: Seq[Exp]) extends MaybeNum with MaybeBool
+case class MapSelect(map: Exp, idxs: Seq[Exp]) extends Lhs with MaybeNum with MaybeBool
 
 // --- Statements
 
 sealed trait Stmt extends Node
-case class Assume(exp: Exp) extends Stmt
-case class Assert(exp: Exp) extends Stmt
+case class Assume(exp: MaybeBool) extends Stmt
+case class Assert(exp: MaybeBool) extends Stmt
 case class Assign(lhs: Lhs, rhs: Exp) extends Stmt
+case class Havoc(vars: Seq[LocalVar]) extends Stmt
+case class Goto(dests: Seq[Lbl]) extends Stmt
+case class If(cond: MaybeBool, thn: Seq[Stmt], els: Seq[Stmt]) extends Stmt
+case class NondetIf(thn: Seq[Stmt], els: Seq[Stmt]) extends Stmt
+/** A single-line comment (s should not contain new-lines) */
+case class Comment(s: String) extends Stmt
+/**
+ * A comment block can be used to group together a sequence of statements that
+ * belong together, as described by a comment.
+ */
+case class CommentBlock(s: String, stmts: Seq[Stmt]) extends Stmt
 
 // --- Declarations
 
+case class Label(lbl: Lbl) extends Stmt
 case class Program(decls: Seq[Decl]) extends Node
 sealed trait Decl extends Node
 case class TypeDecl(name: String) extends Decl
 case class Const(name: String, typ: Type) extends Decl
 case class Func(name: String, args: Seq[LocalVarDecl], typ: Type, typArgs: Seq[TypeVar]) extends Decl
-case class Axiom(exp: Exp) extends Decl
+case class Axiom(exp: MaybeBool) extends Decl
 case class GlobalVarDecl(name: String, typ: Type) extends Decl
-case class Procedure(name: String, ins: Seq[LocalVarDecl], outs: Seq[LocalVarDecl], modifies: Seq[String], pres: Seq[Exp], posts: Seq[Exp], body: Stmt) extends Decl
+case class Procedure(name: String, ins: Seq[LocalVarDecl], outs: Seq[LocalVarDecl], modifies: Seq[String], body: Stmt) extends Decl
