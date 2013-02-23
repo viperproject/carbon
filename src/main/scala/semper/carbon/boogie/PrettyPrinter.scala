fd1712f4ed99919eb1de7408a2e8088b44b4307a
Stefan Heule <stefanheule@gmail.com>
2013-02-23 02:00:47 +0100

    Beginning of a Boogie pretty-printer.

---
+++ src/main/scala/semper/carbon/boogie/PrettyPrinter.scala
@@ -0,0 +1,83 @@
+package semper.carbon.boogie
+
+import org.kiama.output._
+import UnicodeString.lift
+
+/**
+ * A pretty printer for the Boogie AST.
+ *
+ * @author Stefan Heule
+ */
+object PrettyPrinter extends org.kiama.output.PrettyPrinter with ParenPrettyPrinter {
+
+  override val defaultIndent = 2
+  /** Pretty-print any AST node. */
+  def pretty(n: Node): String = {
+    super.pretty(show(n))
+  }
+
+  /** Show any AST node. */
+  def show(n: Node): Doc = {
+    n match {
+      case exp: Exp => toParenDoc(exp)
+      case stmt: Stmt => showStmt(stmt)
+      case typ: Type => showType(typ)
+      case p: Program => showProgram(p)
+      case m: Decl => showDecl(m)
+      case _: LocalVarDecl => sys.error("local variable declarations should be shown with special routines depending on their type")
+    }
+  }
+
+  def showType(t: Type) = {
+    t match {
+      case Int => value("int")
+      case Bool => value("bool")
+      case Real => value("real")
+      case _ => empty
+    }
+  }
+
+  def showStmt(stmt: Stmt) = {
+    empty
+  }
+
+  def showProgram(p: Program) = {
+    empty
+  }
+
+  def showDecl(decl: Decl) = {
+    empty
+  }
+
+  def showVar(variable: LocalVarDecl) = {
+    variable.name <> ":" <+> showType(variable.typ)
+  }
+
+  def showTrigger(t: Trigger) = {
+    "{" <+> ssep(t.exps map show, comma <> space) <+> "}"
+  }
+
+  // Note: pretty-printing expressions is mostly taken care of by kiama
+  override def toParenDoc(e: PrettyExpression): Doc = {
+    e match {
+      case IntLit(i) => value(i)
+      case BoolLit(b) => value(b)
+      case RealLit(d) => value(d)
+      case Forall(vars, triggers, exp) =>
+        parens(("∀" or "forall") <+>
+          ssep(vars map showVar, comma <> space) <+>
+          ("•" or "::") <+>
+          ssep(triggers map showTrigger, space) <+>
+          show(exp))
+      case Exists(vars, triggers, exp) =>
+        parens(("∃" or "exists") <+>
+          ssep(vars map showVar, comma <> space) <+>
+          ("•" or "::") <+>
+          ssep(triggers map showTrigger, space) <+>
+          show(exp))
+      case LocalVar(name, typ) => value(name)
+      case _: PrettyUnaryExpression | _: PrettyBinaryExpression => super.toParenDoc(e)
+    }
+  }
+
+}
