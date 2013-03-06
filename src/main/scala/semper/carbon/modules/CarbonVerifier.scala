3caf66ffc31d7ae9b763174f1ecb491bf88fc14f
Stefan Heule <stefanheule@gmail.com>
2013-03-06 18:34:49 +0100

    CarbonVerifier that takes care of command-line arguments, etc.

---
+++ src/main/scala/semper/carbon/modules/CarbonVerifier.scala
@@ -0,0 +1,74 @@
+package semper.carbon.modules
+
+import semper._
+import sil.ast.Program
+import sil.utility.Paths
+import sil.verifier.Dependency
+
+/**
+ * The main class to perform verification of SIL programs.  Deals with command-line arguments, configuration
+ * of modules and choosing which module implementations to use.
+ *
+ * @author Stefan Heule
+ */
+case class CarbonVerifier() extends carbon.modules.Verifier with sil.verifier.Verifier {
+
+  /** The (unresolved) path where Boogie is supposed to be located. */
+  var _boogiePath: String = "${BOOGIE_EXE}"
+
+  /** The (unresolved) path where Z3 is supposed to be located. */
+  var _z3Path: String = "${Z3_EXE}"
+
+  /** The (resolved) path where Boogie is supposed to be located. */
+  def boogiePath = Paths.resolveEnvVars(_boogiePath)
+
+  /** The (resolved) path where Z3 is supposed to be located. */
+  def z3Path = Paths.resolveEnvVars(_z3Path)
+
+  def name: String = "carbon"
+  def version: String = "1.0"
+  def copyright: String = "(c) 2013 Stefan Heule"
+
+  def commandLineArgs(options: Seq[String]) {}
+
+  lazy val dependencies: Seq[Dependency] = {
+    import scala.sys.process._
+    val unknownVersion = "(?)"
+    List(new Dependency {
+      def name = "Boogie"
+      def version = {
+        def convertStreamToString(is: java.io.InputStream) = {
+          val s = new java.util.Scanner(is).useDelimiter("\\A")
+          if (s.hasNext) s.next() else ""
+        }
+        var res: String = ""
+        def out(input: java.io.InputStream) {
+          res = convertStreamToString(input)
+          input.close()
+        }
+        // Note: call exitValue to block until Boogie has finished
+        // Note: we call boogie with an empty input "file" on stdin and parse the output
+        Seq(boogiePath, "stdin.bpl").run(new ProcessIO(_.close(), out, _.close())).exitValue()
+        if (res.startsWith("Boogie program verifier version ")) {
+          res.substring(0, res.indexOf(",")).substring("Boogie program verifier version ".length)
+        } else {
+          unknownVersion
+        }
+      }
+      def location = boogiePath
+    }, new Dependency {
+      def name = "Z3"
+      def version = {
+        val v = List(z3Path, "/version").lines.to[List]
+        if (v.size == 1 && v(0).startsWith("Z3 version ")) {
+          v(0).substring("Z3 version ".size)
+        } else {
+          unknownVersion
+        }
+      }
+      def location = z3Path
+    })
+  }
+
+  def verify(program: Program) = ???
+}
