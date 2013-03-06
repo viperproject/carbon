54a067a67578e3c94576edda547d7bf071fc2578
Stefan Heule <stefanheule@gmail.com>
2013-03-06 18:34:56 +0100

    Adapt Carbon to new CarbonVerifier.

--- src/main/scala/semper/carbon/Carbon.scala
+++ src/main/scala/semper/carbon/Carbon.scala
@@ -1,28 +1,19 @@
 package semper.carbon
 
-import semper.sil.frontend.SilFrontEnd
-import semper.sil.verifier.{VerificationResult, Verifier}
-import semper.sil.ast.Program
+import modules.CarbonVerifier
+import semper.sil.frontend.SilFrontend
+import semper.sil.verifier.Verifier
 
 /**
  * The main object for Carbon containing the execution start-point.
  *
  * @author Stefan Heule
  */
-object Carbon extends SilFrontEnd {
+object Carbon extends SilFrontend {
 
   def main(args: Array[String]) {
     execute(args)
   }
 
-  def verifier: Verifier = new Verifier() {
-    def verify(program: Program): VerificationResult = null
-    def name = "carbon"
-    def dependencyVersions = Nil
-    def version = "1.0"
-    def copyright = "(c) 2013 Stefan Heule"
-    def commandLineArgs(options: Seq[String]) {
-
-    }
-  }
+  def verifier: Verifier = CarbonVerifier()
 }
