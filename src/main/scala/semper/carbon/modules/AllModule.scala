3f96d21b82a0393258bacf66ee1c65cd078d756f
Stefan Heule <stefanheule@gmail.com>
2013-03-10 14:15:15 +0100

    Better scaladoc.

--- src/main/scala/semper/carbon/modules/AllModule.scala
+++ src/main/scala/semper/carbon/modules/AllModule.scala
@@ -4,9 +4,9 @@ import semper.sil.{ast => sil}
 import semper.carbon.boogie._
 
 /**
- * A trait that has all methods of all modules and forwards calls to the correct module
+ * A trait that has most methods of all modules and forwards calls to the correct module
  * from the verifier.  This is used for convenience to simplify module implementations
- * by allowing to directly call translate and other methods.
+ * by allowing to directly call `translate` and other methods.
  */
 trait AllModule extends Module {
   def translateStmt(s: sil.Stmt): Stmt = verifier.stmtModule.translateStmt(s)
