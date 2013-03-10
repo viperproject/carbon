eeb7bf19a3e2f5304d56cea540d630622287f5b8
Stefan Heule <stefanheule@gmail.com>
2013-03-10 13:52:05 +0100

    Main method for Verifier.

--- src/main/scala/semper/carbon/modules/Verifier.scala
+++ src/main/scala/semper/carbon/modules/Verifier.scala
@@ -1,5 +1,8 @@
 package semper.carbon.modules
 
+import semper.sil.{ast => sil}
+import semper.carbon.boogie.Program
+
 /**
  * A verifier for SIL in Carbon (defines what modules need to be available).
  *
@@ -14,4 +17,13 @@ trait Verifier {
   def heapModule: HeapModule
   def funcPredModule: FuncPredModule
   def permModule: PermModule
+
+  /**
+   * Translate a SIL program into a Boogie program.
+   */
+  def translate(p: sil.Program): Program = {
+    p match {
+      case sil.Program(name, domains, fields, functions, predicates, methods) => Program(Nil)
+    }
+  }
 }
