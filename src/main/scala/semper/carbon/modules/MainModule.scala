d957bf4a688840139a4fd8c88c73c8ad83e4c563
Stefan Heule <stefanheule@gmail.com>
2013-03-12 14:18:04 +0100

    Translate variable declarations.

--- src/main/scala/semper/carbon/modules/MainModule.scala
+++ src/main/scala/semper/carbon/modules/MainModule.scala
@@ -2,6 +2,7 @@ package semper.carbon.modules
 
 import semper.sil.{ast => sil}
 import semper.carbon.boogie._
+import semper.carbon.verifier.Environment
 
 /**
  * A module for translating SIL programs by invoking the right modules and
@@ -14,6 +15,15 @@ trait MainModule extends Module {
    * Translate a SIL program into a Boogie program.
    */
   def translate(p: sil.Program): Program
+
+  /**
+   * Translate a local variable declaration, and register the variable with
+   * the environment.
+   */
+  def translateLocalVarDecl(l: sil.LocalVarDecl): LocalVarDecl
+
+  /** The current environment. */
+  def env: Environment
 
   def translateFunction(f: sil.Function): Seq[Decl]
   def translateDomain(d: sil.Domain): Seq[Decl]
