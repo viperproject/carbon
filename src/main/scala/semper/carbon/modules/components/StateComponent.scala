4ba9f66e96a9718ad8e9c62ae99c58eef44672e0
Stefan Heule <stefanheule@gmail.com>
2013-03-13 19:13:15 +0100

    New StateComponent.

---
+++ src/main/scala/semper/carbon/modules/components/StateComponent.scala
@@ -0,0 +1,29 @@
+package semper.carbon.modules.components
+
+import semper.carbon.boogie.{Exp, LocalVarDecl, Stmt}
+
+/**
+ * The [[semper.carbon.modules.StateModule]] allows to register state components that
+ * contribute to the state of the execution of the program.  In the rest of this class,
+ * we use 'state' to refer to the program state during execution.
+ *
+ * @author Stefan Heule
+ */
+trait StateComponent extends Component {
+
+  /**
+   * The statements necessary to initialize the part of the state belonging to this module.
+   */
+  def initState: Stmt
+
+  /**
+   * The name and type of the contribution of this components to the state.
+   */
+  def stateContributions: Seq[LocalVarDecl]
+
+  /**
+   * The current values for this components state contributions.  The number of elements
+   * in the list and the types must correspond to the ones given in `stateContributions`.
+   */
+  def currentStateContributions: Seq[Exp]
+}
