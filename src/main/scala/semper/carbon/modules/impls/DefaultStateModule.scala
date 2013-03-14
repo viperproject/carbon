58f54b32708e7f5251a008d0dff4fa8d591b3ed4
Stefan Heule <stefanheule@gmail.com>
2013-03-14 16:19:32 +0100

    Define IsGoodState function.

--- src/main/scala/semper/carbon/modules/impls/DefaultStateModule.scala
+++ src/main/scala/semper/carbon/modules/impls/DefaultStateModule.scala
@@ -3,7 +3,8 @@ package semper.carbon.modules.impls
 import semper.carbon.modules._
 import semper.sil.{ast => sil}
 import semper.carbon.verifier.Verifier
-import semper.carbon.boogie.{Seqn, Stmt, LocalVarDecl, Exp}
+import semper.carbon.boogie._
+import semper.carbon.boogie.Implicits._
 
 /**
  * The default implementation of a [[semper.carbon.modules.StateModule]].
@@ -12,8 +13,18 @@ import semper.carbon.boogie.{Seqn, Stmt, LocalVarDecl, Exp}
  */
 class DefaultStateModule(val verifier: Verifier) extends StateModule {
   def name = "State module"
+  private val isGoodState = "IsGoodState"
 
-  def initState: Stmt = Seqn(components map (_.initState))
+  override def preamble = {
+    Func(isGoodState, stateContributions, Bool)
+  }
+
+  def initState: Stmt = {
+    Seqn(
+      (components map (_.initState)) :+
+        Assume(FuncApp(isGoodState, currentStateContributions))
+    )
+  }
   def stateContributions: Seq[LocalVarDecl] = components flatMap (_.stateContributions)
   def currentStateContributions: Seq[Exp] = components flatMap (_.currentStateContributions)
 }
