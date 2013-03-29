6f0abc555a5a6f026cb01b93ea8292cb69a12e51
Stefan Heule <stefanheule@gmail.com>
2013-03-29 09:48:43 +0100

    Delete any temporary files at the end.

--- src/main/scala/semper/carbon/verifier/BoogieInterface.scala
+++ src/main/scala/semper/carbon/verifier/BoogieInterface.scala
@@ -83,6 +83,7 @@ trait BoogieInterface {
     }
     // write program to a temporary file
     val tmp = File.createTempFile("carbon", ".bpl")
+    tmp.deleteOnExit()
     val stream = new BufferedOutputStream(new FileOutputStream(tmp))
     stream.write(input.getBytes)
     stream.close()
