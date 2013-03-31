0148e800ee7ac37c31b9fe96fa869fbcaf4b4230
Stefan Heule <stefanheule@gmail.com>
2013-03-31 15:09:39 +0200

    Better comments for module translation.

--- src/main/scala/semper/carbon/modules/impls/DefaultDomainModule.scala
+++ src/main/scala/semper/carbon/modules/impls/DefaultDomainModule.scala
@@ -24,20 +24,19 @@ class DefaultDomainModule(val verifier: Verifier) extends DomainModule {
   override def translateDomain(domain: sil.Domain): Seq[Decl] = {
     val fs = domain.functions flatMap translateDomainFunction
     val as = domain.axioms flatMap translateDomainAxiom
-    val ts = DeclComment(s"The type for domain ${domain.name}") ::
-      TypeDecl(NamedType(domain.name, domain.typVars map (tv => TypeVar(tv.name)))) ::
-      Nil
-    CommentedDecl(s"Translation of domain ${domain.name}", ts ++ fs ++ as)
+    val ts = CommentedDecl(s"The type for domain ${domain.name}",
+      TypeDecl(NamedType(domain.name, domain.typVars map (tv => TypeVar(tv.name)))), size = 1)
+    CommentedDecl(s"Translation of domain ${domain.name}", ts ++ fs ++ as, nLines = 2)
   }
 
   private def translateDomainFunction(f: sil.DomainFunc): Seq[Decl] = {
     val args = f.formalArgs map (x => LocalVarDecl(Identifier(x.name), translateType(x.typ)))
     val func = Func(Identifier(f.name), args, translateType(f.typ))
-    CommentedDecl(s"Translation of domain function ${f.name}", func)
+    CommentedDecl(s"Translation of domain function ${f.name}", func, size = 1)
   }
 
   private def translateDomainAxiom(axiom: sil.DomainAxiom): Seq[Decl] = {
-    CommentedDecl(s"Translation of domain axiom ${axiom.name}", Axiom(translateExp(axiom.exp)))
+    CommentedDecl(s"Translation of domain axiom ${axiom.name}", Axiom(translateExp(axiom.exp)), size = 1)
   }
 
   override def translateDomainFuncApp(fa: sil.DomainFuncApp): Exp = {
