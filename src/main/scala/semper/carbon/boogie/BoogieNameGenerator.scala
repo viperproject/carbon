6929fbeac66ebb3884f4206d8a8b1455d73a9a7b
Stefan Heule <stefanheule@gmail.com>
2013-04-11 00:16:42 +0200

    Adapt to SIL change.

--- src/main/scala/semper/carbon/boogie/BoogieNameGenerator.scala
+++ src/main/scala/semper/carbon/boogie/BoogieNameGenerator.scala
@@ -1,15 +1,16 @@
 package semper.carbon.boogie
 
-import semper.sil.utility.NameGenerator
+import semper.sil.utility.DefaultNameGenerator
 
 /**
- * 
+ *
  * @author Stefan Heule
  */
-class BoogieNameGenerator extends NameGenerator {
+class BoogieNameGenerator extends DefaultNameGenerator {
   // in principle, some others would also be allowed, but this is good enough for now
   private val otherChars = "_.$#'`~^\\?a-zA-Z"
   def firstCharacter = s"[$otherChars]".r
   def otherCharacter = s"[0-9$otherChars]".r
   def separator = "_"
+  def reservedNames: Set[String] = Set()
 }
