af8ef8b47187a6cc7a40acfba52ce3ed8ea42af2
Stefan Heule <stefanheule@gmail.com>
2013-04-13 12:12:48 +0200

    Initialize old(Mask).

--- src/main/scala/semper/carbon/modules/impls/DefaultPermModule.scala
+++ src/main/scala/semper/carbon/modules/impls/DefaultPermModule.scala
@@ -146,7 +146,8 @@ class DefaultPermModule(val verifier: Verifier)
   def stateContributions: Seq[LocalVarDecl] = Seq(LocalVarDecl(maskName, maskType))
   def currentStateContributions: Seq[Exp] = Seq(mask)
   def initState: Stmt = {
-    (mask := zeroMask)
+    (mask := zeroMask) ++
+      Assume(Old(mask) === mask)
   }
 
   private def permAdd(a: Exp, b: Exp): Exp = FuncApp(permAddName, Seq(a, b), permType)
