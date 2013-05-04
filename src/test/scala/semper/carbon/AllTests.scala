5f5bad6e73799ab8897b419de9eb1d71207098e9
Stefan Heule <stefanheule@gmail.com>
2013-05-04 14:32:15 +0200

    Adapt to SIL changes.

--- src/test/scala/semper/carbon/AllTests.scala
+++ src/test/scala/semper/carbon/AllTests.scala
@@ -5,6 +5,7 @@ import semper.sil.verifier.Verifier
 import semper.sil.frontend.Frontend
 import java.io.File
 import io.Source
+import java.nio.file.Path
 
 /** All tests for carbon.
   *
@@ -14,7 +15,7 @@ class AllTests extends DefaultSilSuite {
 
   override def testDirectories: Seq[String] = Vector("all")
 
-  override def frontend(verifier: Verifier, files: Seq[File]): Frontend = {
+  override def frontend(verifier: Verifier, files: Seq[Path]): Frontend = {
     require(files.length == 1, "tests should consist of exactly one file")
     val fe = new CarbonFrontend()
     fe.init(verifier)
