5ecae7d1c3859af3886e6ecbf808d72876b5fe37
Stefan Heule <stefanheule@gmail.com>
2013-06-26 10:47:18 +0200

    Transformer for Boogie AST.

---
+++ src/main/scala/semper/carbon/boogie/Transformer.scala
@@ -0,0 +1,87 @@
+package semper.carbon.boogie
+
+
+/**
+ * An implementation for transformers of the Boogie AST.
+ *
+ * @author Stefan Heule
+ */
+object Transformer {
+
+  def transform[A <: Node](node: A,
+                           pre: PartialFunction[Node, Node] = PartialFunction.empty)(
+                            recursive: Node => Boolean = !pre.isDefinedAt(_),
+                            post: PartialFunction[Node, Node] = PartialFunction.empty): A = {
+    def go[B <: Node](root: B): B = {
+      transform(root, pre)(recursive, post)
+    }
+
+    def recurse(parent: Node): Node = {
+      parent match {
+        case Program(header, decls) =>
+          Program(header, decls map go)
+        case LocalVarDecl(name, typ, Some(where)) =>
+          LocalVarDecl(name, go(typ), Some(go(where)))
+        case LocalVarDecl(name, typ, None) =>
+          LocalVarDecl(name, go(typ), None)
+        case Trigger(exps) => Trigger(exps map go)
+        case t: Type => parent
+        case d: Decl =>
+          d match {
+            case ConstDecl(name, typ, unique) => ConstDecl(name, go(typ), unique)
+            case TypeDecl(name) => parent
+            case TypeAlias(n, de) => TypeAlias(go(n), go(de))
+            case Func(name, args, typ) => Func(name, args map go, go(typ))
+            case Axiom(exp) => Axiom(go(exp))
+            case GlobalVarDecl(name, typ) => GlobalVarDecl(name, go(typ))
+            case Procedure(name, ins, outs, body) => Procedure(name, ins map go, outs map go, go(body))
+            case CommentedDecl(s, ds, a, b) => CommentedDecl(s, ds map go, a, b)
+            case DeclComment(s) => parent
+            case LiteralDecl(s) => parent
+          }
+        case ss: Stmt =>
+          ss match {
+            case Assign(lhs, rhs) => Assign(go(lhs), go(rhs))
+            case Assert(e, error) => Assert(go(e), error)
+            case Assume(e) => Assume(go(e))
+            case HavocImpl(es) => HavocImpl(es map go)
+            case Comment(s) => parent
+            case CommentBlock(s, stmt) => CommentBlock(s, go(stmt))
+            case Seqn(s) => Seqn(s map go)
+            case If(cond, thn, els) => If(go(cond), go(thn), go(els))
+            case NondetIf(thn, els) => NondetIf(go(thn), go(els))
+            case Label(name) => parent
+            case Goto(target) => parent
+            case LocalVarWhereDecl(idn, where) => LocalVarWhereDecl(idn, go(where))
+          }
+        case e: Exp =>
+          // Note: If you have to update this pattern match to make it exhaustive, it
+          // might also be necessary to update the PrettyPrinter.toParenDoc method.
+          e match {
+            case IntLit(i) => parent
+            case BoolLit(b) => parent
+            case RealLit(b) => parent
+            case LocalVar(n, t) => LocalVar(n, go(t))
+            case GlobalVar(n, t) => GlobalVar(n, go(t))
+            case Const(i) => parent
+            case MapSelect(map, idxs) => MapSelect(go(map), idxs map go)
+            case Old(exp) => Old(go(exp))
+            case CondExp(cond, thn, els) => CondExp(go(cond), go(thn), go(els))
+            case Exists(v, exp) => Exists(v map go, go(exp))
+            case Forall(v, triggers, exp) => Forall(v map go, triggers map go, go(exp))
+            case BinExp(left, binop, right) => BinExp(go(left), binop, go(right))
+            case UnExp(unop, exp) => UnExp(unop, go(exp))
+            case FuncApp(func, args, typ) => FuncApp(func, args map go, go(typ))
+          }
+      }
+    }
+
+    val beforeRecursion = pre.applyOrElse(node, identity[Node])
+    val afterRecursion = if (recursive(node)) {
+      recurse(beforeRecursion)
+    } else {
+      beforeRecursion
+    }
+    post.applyOrElse(afterRecursion, identity[Node]).asInstanceOf[A]
+  }
+}
