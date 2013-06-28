baa3bc52dee4615e9ae7c81d6f596646611a50b3
Stefan Heule <stefanheule@gmail.com>
2013-06-28 19:03:56 +0200

    Fix missing parens.

--- src/main/scala/semper/carbon/modules/impls/DefaultHeapModule.scala
+++ src/main/scala/semper/carbon/modules/impls/DefaultHeapModule.scala
@@ -148,7 +148,7 @@ class DefaultHeapModule(val verifier: Verifier) extends HeapModule with SimpleSt
           // assume that we consider a newly allocated cell, which gives the prover
           // the information that this object is different from anything allocated
           // earlier.
-          Assume(freshObjectVar !== nullLit && alloc(freshObjectVar).not) ::
+          Assume((freshObjectVar !== nullLit) && alloc(freshObjectVar).not) ::
           (alloc(freshObjectVar) := TrueLit()) ::
           (translateExp(target) := freshObjectVar) ::
           Nil
