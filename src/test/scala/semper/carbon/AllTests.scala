a2de07e47e6447d09d272632efed918f17cc2d50
Stefan Heule <stefanheule@gmail.com>
2013-07-02 12:46:53 +0200

    Remove generated tests again.

--- src/test/scala/semper/carbon/AllTests.scala
+++ src/test/scala/semper/carbon/AllTests.scala
@@ -13,7 +13,9 @@ import java.nio.file.Path
   */
 class AllTests extends DefaultSilSuite {
 
-  override def testDirectories: Seq[String] = Vector("all", "local", "generated")
+  override def testDirectories: Seq[String] = Vector("all", "local"
+    //, "generated"
+  )
 
   override def frontend(verifier: Verifier, files: Seq[Path]): Frontend = {
     require(files.length == 1, "tests should consist of exactly one file")
