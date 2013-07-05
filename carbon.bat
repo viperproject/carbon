347127f6428dad8d80a5fff012acb31d2022318b
Stefan Heule <stefanheule@gmail.com>
2013-07-06 01:08:54 +0200

    Launch script.

---
+++ carbon.bat
@@ -0,0 +1,45 @@
+@echo off
+SetLocal EnableDelayedExpansion
+
+set ROOT_DIR=%~dp0
+set JAVA_EXE=java
+
+if "%IVY_HOME%"=="" (
+	REM Environment variable IVY_HOME not pre-defined; checks on classpath below will catch a false definition here
+	set IVY_HOME=%USERPROFILE%\.ivy2
+)
+
+set SCALA_VERSION=2.10.0
+set SCALA_VRS=2.10
+
+set __CP.SCALA_LIB="%IVY_HOME%\cache\org.scala-lang\scala-library\jars\scala-library-%SCALA_VERSION%.jar"
+set __CP.KIAMA_LIB="%IVY_HOME%\cache\com.googlecode.kiama\kiama_%SCALA_VRS%\jars\kiama_%SCALA_VRS%-1.5.0.jar"
+set __CP.SCALOP_LIB="%IVY_HOME%\cache\org.rogach\scallop_%SCALA_VRS%\jars\scallop_%SCALA_VRS%-0.8.1.jar"
+set __CP.JGRAPH_LIB="%IVY_HOME%\cache\org.jgrapht\jgrapht-jdk1.5\jars\jgrapht-jdk1.5-0.7.3.jar"
+set __CP.CARBON="%ROOT_DIR%target\scala-%SCALA_VRS%\classes"
+set __CP.SIL="%ROOT_DIR%..\sil\target\scala-%SCALA_VRS%\classes"
+
+set CP=
+for /f "tokens=2* delims=.=" %%A in ('set __CP.') do (
+	if not exist %%B (
+		echo %%B does not exist.
+		goto :exit_with_error
+	) else (
+		set CP=!CP!;%%B
+	)
+)
+
+set CARBON_MAIN=semper.carbon.Playground
+
+set CARBON_OPTS=verify
+set CARBON_OPTS=%CARBON_OPTS% %*
+
+::-Xss16M 
+set CMD=%JAVA_EXE% -Xss30M -cp %CP% %CARBON_MAIN% %CARBON_OPTS%
+
+call %CMD%
+
+exit /B 0
+
+:exit_with_error
+exit /B 1
