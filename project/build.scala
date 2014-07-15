64f5ba655c752536d0b46e0b50dd56dea364de4a
Alexander J. Summers <alexander.summers@inf.ethz.ch>
2014-07-15 17:37:19 +0200

    renamed a spare occurrence of semper

--- project/build.scala
+++ project/build.scala
@@ -47,7 +47,7 @@ object CarbonBuild extends Build {
   }
 
   object dependencies {
-    lazy val sil = "semper" %% "silver" %  "0.1-SNAPSHOT" % "compile->compile;test->test"
+    lazy val sil = "viper" %% "silver" %  "0.1-SNAPSHOT" % "compile->compile;test->test"
     lazy val silSrc = RootProject(new java.io.File("../silver"))
   }
 }
