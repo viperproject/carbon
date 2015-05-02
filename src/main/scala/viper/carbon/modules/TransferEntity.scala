e354f7c0b8bab51396d1afa66d1e256548eb2385
Gaurav Parthasarathy <gaurav.parthasarathy@inf.ethz.ch>
2015-05-02 19:46:16 +0200

    allow TransferableEntity to return custom VerificationError

--- src/main/scala/viper/carbon/modules/TransferEntity.scala
+++ src/main/scala/viper/carbon/modules/TransferEntity.scala
@@ -1,6 +1,7 @@
 package viper.carbon.modules
 
 import viper.carbon.boogie.{RealLit, Exp}
+import viper.silver.verifier.{PartialVerificationError, reasons, VerificationError}
 import viper.silver.{ast => sil}
 
 /**
@@ -10,12 +11,23 @@ sealed trait TransferableEntity {
   def rcv: Exp
   def loc: Exp
   def transferAmount: Exp
+  def originalSILExp: sil.Exp
+
+  def transferError(error: PartialVerificationError):VerificationError
 }
 
 object TransferableEntity {
   def unapply(te: TransferableEntity) = Some((te.rcv,te.loc,te.transferAmount))
 }
 
-case class TransferableAccessPred(rcv: Exp, loc:Exp, transferAmount: Exp) extends TransferableEntity
+case class TransferableAccessPred(rcv: Exp, loc:Exp, transferAmount: Exp,originalSILExp: sil.AccessPredicate) extends TransferableEntity {
+  override def transferError(error: PartialVerificationError):VerificationError = {
+    error.dueTo(reasons.InsufficientPermission(originalSILExp.loc))
+  }
+}
 
-case class TransferableWand(rcv:Exp, loc: Exp, transferAmount: Exp = RealLit(1.0)) extends TransferableEntity
+case class TransferableWand(rcv:Exp, loc: Exp, transferAmount: Exp,originalSILExp: sil.MagicWand) extends TransferableEntity {
+  override def transferError(error: PartialVerificationError): VerificationError = {
+    error.dueTo(reasons.MagicWandChunkNotFound(originalSILExp))
+  }
+}
