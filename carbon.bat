a06c9ab9c0e4e758000c06ea13e1dab78082364f
Frederik Rothenberger <frothenb@student.ethz.ch>
2015-09-09 20:20:54 +0200

    Improved comments in carbon.bat.

--- carbon.bat
+++ carbon.bat
@@ -2,17 +2,18 @@
 
 set JAVA_EXE=java
 
-:: only call sbt if the classpath file is missing
+:: Only call sbt if the classpath file is missing.
 if not exist carbon_classpath.txt (
-	rem read all lines of the sbt runtime classpath output and parse it against the regex supplied to findstr
-	rem the regex looks for lines not starting with '[' and ending in '.jar' as the classpath usually does
-	rem (and log lines don't, since they are prefixed with [LOGLEVEL])
+	rem Read all lines of the sbt runtime classpath output and parse it against the regex supplied to findstr.
+	rem The regex looks for lines not starting with '[' and ending in '.jar' as the classpath usually does
+	rem (and log lines don't, since they are prefixed with [LOGLEVEL]).
 	for /f "tokens=*" %%i in ('sbt "export runtime:dependencyClasspath" ^| findstr "[^\[].*\.jar$"') do (
 		echo |set /p=%%i > carbon_classpath.txt
 	)
 )
 
-:: read classpath file in rather cumbersome way to avoid the 1024 character buffer limit
+:: Read classpath file in rather cumbersome way to avoid the 1024 character buffer limit.
+:: Note: this solutions breaks, once the classpath is longer than 8192 characters!
 for /f "delims=" %%x in (carbon_classpath.txt) do set CP=%%x
 
 set CARBON_MAIN=viper.carbon.Carbon
