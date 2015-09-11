db44c48caacc1b054c21f28aba790f62b7e2a359
Vytautas Astrauskas <vastrauskas@gmail.com>
2015-09-11 12:41:52 +0200

    Bumped Scala version to 2.11.7.

--- project/build.scala
+++ project/build.scala
@@ -9,7 +9,7 @@ object CarbonBuild extends Build {
     ++ Seq(
           organization := "viper",
           version := "1.0-SNAPSHOT",
-          scalaVersion := "2.11.4",
+          scalaVersion := "2.11.7",
           scalacOptions in Compile ++= Seq("-deprecation", "-unchecked", "-feature"),
           libraryDependencies += "org.rogach" %% "scallop" % "0.9.5",
           libraryDependencies += "org.jgrapht" % "jgrapht-core" % "0.9.0"
