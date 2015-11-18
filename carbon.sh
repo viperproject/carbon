def2fde2c858161fdddcbc9f7ea33e7be2478256
Florian Hahn <flo@fhahn.com>
2015-11-18 11:34:59 +0100

    Allow caller of carbon.sh to set BOOGIE_EXE and Z3_EXE and carbon.sh can now be called from everywhere

--- carbon.sh
+++ carbon.sh
@@ -9,7 +9,19 @@
 # Ensure that the file 'Boogie.exe' in the Boogie folder is executable.
 # Note that the 'z3.exe' file must be located inside the Boogie folder.
 
-export BOOGIE_EXE=$BOOGIE_HOME/Boogie.exe
-export Z3_EXE=$BOOGIE_HOME/z3.exe
 
-sbt "run-main viper.carbon.Carbon $@"
+BOOGIE_EXE="${BOOGIE_EXE:-$BOOGIE_HOME/Boogie.exe}"
+Z3_EXE="${Z3_EXE:-$BOOGIE_HOME/z3.exe}"
+
+export Z3_EXE
+export BOOGIE_EXE
+
+BASEDIR="$(realpath `dirname $0`)"
+
+CP_FILE="$BASEDIR/carbon_classpath.txt"
+
+if [ ! -f $CP_FILE ]; then
+    (cd $BASEDIR; sbt "export runtime:dependencyClasspath" | tail -n1 > $CP_FILE)
+fi
+
+java -Xss30M -cp "`cat $CP_FILE`" viper.carbon.Carbon $@
