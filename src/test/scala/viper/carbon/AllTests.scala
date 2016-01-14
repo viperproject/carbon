b81ca8026ba55465a6a4c0125f48946bb11ad407
Alexander J. Summers <alexander.summers@inf.ethz.ch>
2016-01-14 11:42:58 +0100

    added all test directories

--- src/test/scala/viper/carbon/AllTests.scala
+++ src/test/scala/viper/carbon/AllTests.scala
@@ -18,7 +18,7 @@ import java.nio.file.Path
   */
 class AllTests extends SilSuite {
 
-  override def testDirectories: Seq[String] = Vector("wands", "all", "local"
+  override def testDirectories: Seq[String] = Vector("wands", "all", "local", "quantifiedpermissions"
     //, "generated"
   )
 
