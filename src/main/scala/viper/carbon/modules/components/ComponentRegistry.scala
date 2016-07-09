5231f9a0dab858be1c1ce08fd0bd5a1b1889428e
Alexander J. Summers <alexander.summers@inf.ethz.ch>
2016-07-09 17:19:50 +0200

    Refactored ComponentRegistry code. State is not in the registries, not in the components (which didn't work, since a module can be many kinds of component), and cycles in the ordering are detected properly and cause a runtime exception.

--- src/main/scala/viper/carbon/modules/components/ComponentRegistry.scala
+++ src/main/scala/viper/carbon/modules/components/ComponentRegistry.scala
@@ -9,41 +9,48 @@ package viper.carbon.modules.components
 import viper.carbon.utility.PartialSort
 import viper.carbon.modules.Module
 
+import scala.collection.mutable
+
 /**
  * A trait to allow registering of components.
 
  */
 trait ComponentRegistry[C <: Component] {
 
-  protected var _components: Seq[C] = Nil
+   var _components: Seq[C] = Nil
+   val beforePairs: mutable.HashSet[(Component,Component)] = mutable.HashSet.empty[(Component,Component)]
 
   /** All currently registered components. */
+  /** NOTE: this should only be called once all components have been added! */
   lazy val components: Seq[C] = {
-    PartialSort.sort(_components, ordering).asInstanceOf[Seq[C]]
+    // complete transitive closure of beforePairs (Warshall's algorithm)
+    for(c <- _components) {
+      for (x <- _components; y <- _components if x != c && y != c && beforePairs.contains(x,c) && beforePairs.contains(c,y)) {
+        beforePairs.add((x,y))
+      }
+    }
+    PartialSort.sort(_components, ordering)
   }
 
   /** Register a new component. */
-  def register(component: C, before: Seq[Any] = Nil, after: Seq[Any] = Nil) {
+  def register(component: C, before: Seq[Component] = Nil, after: Seq[Component] = Nil) {
     _components = _components :+ component
-    component.comesBefore = before
-    component.comesAfter = after
+    beforePairs.add(component,component)
+    for(c <- before) beforePairs.add((c,component))
+    for(c <- after) beforePairs.add((component,c))
   }
 
   /** The partial ordering of components based on before and after. */
-  def ordering: PartialOrdering[Any] = new PartialOrdering[Any] {
-    def tryCompare(x: Any, y: Any): Option[Int] = {
+  def ordering: PartialOrdering[C] = new PartialOrdering[C] {
+    def tryCompare(x: C, y: C): Option[Int] = {
       if (x == y) Some(0)
-      else if (lteq(x, y)) Some(-1)
+      else if (lteq(x, y)) (if (lteq(y,x)) sys.error("Internal Error: Carbon modules are incorrectly configured - module " + x.toString + " is in cyclic ordering with module " + y.toString + "\n" + "The complete ordering was: " + beforePairs.toString) else Some(-1))
       else if (lteq(y, x)) Some(1)
       else None
     }
-    def lteq(x: Any, y: Any): Boolean = {
+    def lteq(x: C, y: C): Boolean = {
       if (x == y) return true
-      if (!(x.isInstanceOf[Component] && y.isInstanceOf[Component])) return false
-      val xc = x.asInstanceOf[Component]
-      val yc = y.asInstanceOf[Component]
-      if (xc.comesBefore.contains(yc)) return true
-      if (yc.comesAfter.contains(xc)) return true
+      if (beforePairs.contains((x,y))) (if(beforePairs.contains((y,x))) sys.error("Internal Error: Carbon modules are incorrectly configured - module " + x.toString + " is in cyclic ordering with module " + y.toString + "\n" + "The complete ordering was: " + beforePairs.toString) else return true)
       false
     }
   }
