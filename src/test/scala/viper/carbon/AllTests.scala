ca53e51253d79068cf5524b71bfb7040793adf00
Malte Schwerhoff <malte.schwerhoff@inf.ethz.ch>
2016-09-23 12:27:47 +0200

    Added our Viper-online examples to Carbon

--- src/test/scala/viper/carbon/AllTests.scala
+++ src/test/scala/viper/carbon/AllTests.scala
@@ -18,7 +18,7 @@ import java.nio.file.Path
   */
 class AllTests extends SilSuite {
 
-  override def testDirectories: Seq[String] = Vector("wands", "all", "local", "quantifiedpermissions"
+  override def testDirectories: Seq[String] = Vector("wands", "all", "local", "quantifiedpermissions", "examples"
     //, "generated"
   )
 
