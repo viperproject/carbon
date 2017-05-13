d7a2bf1441a8b966663d1841f377c8572794ef1b
Alexander J. Summers <alexander.summers@inf.ethz.ch>
2017-05-13 14:35:14 +0200

    Avoid using null when creating unexpected Boogie error nodes (use new DummyNode instead)

--- src/main/scala/viper/carbon/verifier/BoogieInterface.scala
+++ src/main/scala/viper/carbon/verifier/BoogieInterface.scala
@@ -7,15 +7,17 @@
 package viper.carbon.verifier
 
 import viper.silver.verifier._
+
 import sys.process._
 import java.io._
+
 import viper.carbon.boogie._
 import viper.silver.verifier.Failure
-import viper.silver.verifier.errors.PositionedNode
+import viper.silver.verifier.errors.{DummyNode, PositionedNode}
 import viper.silver.verifier.reasons.InternalReason
 import viper.carbon.boogie.Assert
 import viper.carbon.boogie.Program
-import viper.silver.ast.{Positioned, NoPosition, Position}
+import viper.silver.ast.{NoPosition, Position, Positioned}
 
 class BoogieDependency(_location: String) extends Dependency {
   def name = "Boogie"
@@ -86,12 +88,12 @@ trait BoogieInterface {
 
           def id: String = "unknown"
 
-          def offendingNode = null
+          def offendingNode = DummyNode
 
           def withNode(offendingNode: PositionedNode = this.offendingNode) = this.clone().asInstanceOf[ErrorReason]
         }
 
-        def offendingNode = null
+        def offendingNode = DummyNode
 
         override def pos = NoPosition
 
