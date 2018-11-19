3c34e0a67c8660a899a058a5d55f0b2789dd2fc3
Arshavir Ter-Gabrielyan <ter-gabrielyan@inf.ethz.ch>
2018-11-19 16:02:16 +0100

    Add long running test suite LongRunningGraphTests

---
+++ runAllGraphTests.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+BOOGIE_EXE=/usr/local/Viper/boogie/Binaries/Boogie
+Z3_EXE=/usr/local/Viper/z3/bin/z3
+
+export Z3_EXE
+export BOOGIE_EXE
+
+sbt "test:runMain org.scalatest.tools.Runner -o -s viper.carbon.LongRunningGraphTests"
