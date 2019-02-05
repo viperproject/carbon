c47489a0bf38e0418878df477bd5419a2244455c
Fábio Pakk Selmi-Dei <fabio.pakk@inf.ethz.ch>
2019-02-05 14:27:46 +0100

    Removed many warnings from compilation.

--- src/main/scala/viper/carbon/modules/InhaleModule.scala
+++ src/main/scala/viper/carbon/modules/InhaleModule.scala
@@ -8,7 +8,7 @@ package viper.carbon.modules
 
 import components.{ComponentRegistry, InhaleComponent}
 import viper.silver.{ast => sil}
-import viper.carbon.boogie.{Exp, LocalVar, Stmt, TrueLit}
+import viper.carbon.boogie.Stmt
 
 /**
  * A module for translating inhale statements.  The module takes care of the basic
