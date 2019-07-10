7189c9e1509accc0f073af65adbc51aeb5ee76fc
Arshavir Ter-Gabrielyan <ter-gabrielyan@inf.ethz.ch>
2019-07-10 23:04:21 +0200

    Add documntation for PortableCarbonTests; customize the name of this test suite

--- src/test/scala/viper/carbon/PortableCarbonTests.scala
+++ src/test/scala/viper/carbon/PortableCarbonTests.scala
@@ -17,10 +17,48 @@ import viper.silver.verifier.Verifier
 
 
 /**
-  * Currently, we do not run the graph tests by default. This could be easily changed later by removing the
-  * [[DoNotDiscover]] annotation.
-  */
-@DoNotDiscover
+  * This test mechanism is intended for running non-default test suites,
+  * in a portable way. Example run command:
+  *
+  * ```
+  * Z3_EXE=z3.exe
+  * BOOGIE_EXE=Boogie.exe
+  * CARBONTESTS_TARGET=./target
+  * CARBONTESTS_WARMUP=./warmup
+  * CARBONTESTS_REPETITIONS=5
+  * sbt "test:runMain org.scalatest.tools.Runner -o -s viper.carbon.PortableCarbonTests"
+  * ```
+  *
+  * The command above will:
+  * 1. Warm-up the JVM by verifying all .vpr files in ./warmup
+  * 2. Measure time of 5 runs of each .vpr file in ./target
+  * 3. Discard ("trim") the slowest and the fastest runs and compute
+  *   - the mean
+  *   - the best
+  *   - the median
+  *   - the worst
+  *   run times of all these tests, and
+  * 4. Print the timing info (per phase) into STDOUT.
+  * 5. Create JAR files (e.g., target/scala-2.12/carbon_2.12-1.1-SNAPSHOT.jar,
+  *                            target/scala-2.12/carbon_2.12-1.1-SNAPSHOT-tests.jar)
+  *    that can be used to run tests with SBT without the need to distribute/ recompile
+  *    the Viper sources. To run the test without recompiling the sources, these
+  *    JAR files should be put into a directory test-location/lib/
+  *    where test-location/ is the directory where you invoke SBT via:
+  *    ```
+  *    sbt "set trapExit := false" \
+  *    "test:runMain org.scalatest.tools.Runner -o -s viper.carbon.PortableCarbonTests"
+  *    ```
+  *    Note that this command takes the same environment variables as above.
+  *
+  * Note that the warmup and the target must be disjoint (not in a sub-directory relation).
+  *
+  * The default values for environment variables above are:
+  *   - CARBONTESTS_TARGET = ???    // This must be set before invoking SBT!
+  *   - CARBONTESTS_WARMUP = None   // If not specified, skip JVM warmup phase.
+  *   - CARBONTESTS_REPETITIONS = 1 // If less then 3, no "trimming" will happen.
+  *
+  */@DoNotDiscover
 class PortableCarbonTests extends SilSuite with StatisticalTestSuite {
 
   override def verifier = CarbonVerifier()
@@ -33,6 +71,7 @@ class PortableCarbonTests extends SilSuite with StatisticalTestSuite {
     fe
   }
 
+  override def name = "Carbon Statistics"
   override def warmupLocationEnvVarName = "CARBONTESTS_WARMUP"
   override def targetLocationEnvVarName = "CARBONTESTS_TARGET"
 
