2f2b33304021f86dee838a3ea9709a8f4ce77a70
gauravpartha <gaurav.parthasarathy@inf.ethz.ch>
2021-06-29 15:15:37 +0200

    update copyright to 2021

--- src/main/scala/viper/carbon/modules/impls/sequence_axioms/SetAxiomatization.scala
+++ src/main/scala/viper/carbon/modules/impls/sequence_axioms/SetAxiomatization.scala
@@ -2,7 +2,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 //
-// Copyright (c) 2011-2019 ETH Zurich.
+// Copyright (c) 2011-2021 ETH Zurich.
 
 /**
 ; These axioms are based on the DafnyPrelude.bpl file of Microsoft's Dafny tool.
