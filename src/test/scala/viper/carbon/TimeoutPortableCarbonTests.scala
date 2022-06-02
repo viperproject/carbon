3d7b8ce2c58100f86295013f53894a9c7a8a10cb
David Ewert <dewert10@gmail.com>
2022-06-01 17:04:38 -0700

    Fixed TimeoutPortableCarbonTests to take repetitions into account

--- src/test/scala/viper/carbon/TimeoutPortableCarbonTests.scala
+++ src/test/scala/viper/carbon/TimeoutPortableCarbonTests.scala
@@ -4,7 +4,7 @@ import org.scalatest.concurrent.{Signaler, TimeLimitedTests}
 import org.scalatest.time.{Minutes, Span}
 
 class TimeoutPortableCarbonTests extends PortableCarbonTests with TimeLimitedTests {
-  override def timeLimit: Span = Span(5, Minutes)
+  override def timeLimit: Span = Span(5*repetitions, Minutes)
 
   override val defaultTestSignaler: Signaler = Signaler(t => t.interrupt())
 }
