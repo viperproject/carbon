6ab496eff4bd1653179050b033190251fb4a4a0d
gauravpartha <gaurav.parthasarathy@inf.ethz.ch>
2022-12-19 14:37:09 +0100

    Try Boogie SMT name normalization option

--- src/main/scala/viper/carbon/verifier/BoogieInterface.scala
+++ src/main/scala/viper/carbon/verifier/BoogieInterface.scala
@@ -50,6 +50,7 @@ trait BoogieInterface {
   def defaultOptions = Seq("/vcsCores:" + java.lang.Runtime.getRuntime.availableProcessors,
     "/errorTrace:0",
     "/errorLimit:10000000",
+    "/normalizeNames:1",
     "/proverOpt:O:smt.AUTO_CONFIG=false",
     "/proverOpt:O:smt.PHASE_SELECTION=0",
     "/proverOpt:O:smt.RESTART_STRATEGY=0",
