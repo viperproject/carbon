c0940f8e7dfd8bef4d2f95db3ad71a3256835c5a
Federico Poli <federpoli@gmail.com>
2022-12-20 15:20:03 +0100

    Change order of tests

--- src/test/scala/viper/carbon/AllTests.scala
+++ src/test/scala/viper/carbon/AllTests.scala
@@ -28,7 +28,7 @@ class AllTests extends SilSuite {
     // "all/impure_assume",
     // "all/inhale_exhale",
     // "all/invariants",
-    "all/issues/carbon",
+    // "all/issues/carbon",
     "all/issues/silicon",
     "all/issues/silver",
     // "all/macros",
